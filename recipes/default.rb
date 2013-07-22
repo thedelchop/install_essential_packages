04bae700d2ab1a60453abc3c3dbc5b585a3ab286
Joseph DelCioppio <joseph.delcioppio@gmail.com>
2013-07-22 10:05:25 -0400

    Make sure that we are setting the server locale
    PG doesn't work if the server locale isn't setup correctly so make sure
    it gets sets to en_US.utf8

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,7 @@
+execute 'Setup localte as US UTF8' do
+  command "update-locale LANG='en_US.utf8' LC_ALL='en_US.utf8'"
+end
+
 node.default[:packages].each do |p|
   package p do
     action :install
